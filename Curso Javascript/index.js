e6266063dea2b2b151cef30da78928d0fe3122b6
xixo0619 <xixojabm@gmail.com>
2020-06-12 23:12:03 -0600

    Commit

---
+++ Curso Javascript/index.js
@@ -0,0 +1,19 @@
+document.write("<h1>ACM1PT</h1>")
+
+console.error("ERROR ALV")
+console.log({"username": 'ryan', "score": 70});
+
+
+//Bucles
+
+let count = 50
+while(count > 0){
+    console.log('ACM1PT');
+    count = count-1;
+}
+
+
+
+
+
+
